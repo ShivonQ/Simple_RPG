4639dc889b7b627db7f2112fa81c91b6de913063
ShivonQ <realitykeeper@gmail.com>
2016-09-20 13:31:22 -0500

    Simple RPG created using Agile Current Goals: -Character Class (attack_enemy, take_damage) --Hero (Some Methods Done: LevelUp, GainXP) --Monster
    -DB class to handle db stuff
    --Fetch_Monster method will get a monster within the range of level and then make a Monster class instance.
    
    -Game Class
    --Will run the loops for the game
    
    -Encounters Class
    --Combat
    --Resting

---
+++ character/hero.py
@@ -0,0 +1,38 @@
+import Character
+import data
+class Hero(Character):
+    def __init__(self):
+        Character.__init__(self)
+        # in the future a additional Class class will grant further inheritence
+        # Which will make many of these happen there, not here.
+        self.state = 'normal'
+        self.hp = 10
+        self.max_hp = 10
+        self.armor = 2  # Used to figure out if you missed
+        self.strength = 3  # Used for the max on damage
+        self.xp = 0
+        self.level = 1
+        self.money = 5
+        self.next_level = 100
+
+    def gain_xp(self, xp):
+        self.xp += xp
+        print('{} gained {} xp!'.format(self.name, xp))
+
+    def gain_level(self):
+        # Sort through the Dictionary of Levels,
+        levels = data.get_levels()
+        next_level_xp = levels[self.level+1]
+        # check for if the next hero level has been reached.
+        if next_level_xp <= self.xp:
+            self.level += 1
+            print('{} Gained a Level! {} is now level {}!\n')
+            self.next_level = next_level_xp
+    #TODO:   THIS WILL ALSO INCREASE VARIOUS ABILITIES FOR THE HERO
+
+    def status(self):
+        print('_________HERO_STATUS__________\n'
+              ' HP {}/{}   Level {}   XP {}/{}\n'
+              '______________________________'
+              .format(self.hp, self.max_hp, self.level,
+                      self.xp, self.next_level))
