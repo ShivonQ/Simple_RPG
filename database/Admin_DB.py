3c1b8d56fafe946b9d1da23b091586694fd634e1
ShivonQ <realitykeeper@gmail.com>
2016-09-25 15:29:48 -0500

    made most commandline DB functions(should be easy to use in Robs TKINTER if we need to., delete, show, create, modify

--- database/Admin_DB.py
+++ database/Admin_DB.py
@@ -17,6 +17,8 @@ def Admin_DB():
         elif choice == 3:
             delete_hero_loop()
         elif choice == 4:
+            create_admin_loop()
+        elif choice == 5:
             sys.exit()
         else:
             print('That was not an option.')
@@ -31,14 +33,18 @@ def login_loop():
         valid_or_not = db.check_for_admin_priveleges(u_name,pw)
     Admin_DB()
 
+
 def insert_monster_loop():
     print('Here we insert a monster')
+
 
 def delete_monster_loop():
     print('Here we Delete a monster')
 
+
 def delete_hero_loop():
     print('Here we delete a hero')
+
 
 def create_admin_loop():
     print('here we make more admins')
