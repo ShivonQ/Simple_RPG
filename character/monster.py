083ee375ed5b7ba4c0559b40d5e35a8f06d28a26
ShivonQ <realitykeeper@gmail.com>
2016-09-26 12:05:54 -0500

    Last push for today

--- character/monster.py
+++ character/monster.py
@@ -1,7 +1,8 @@
 import Character
 
+
 class Monster(Character):
-    def __init__(self, name, max_hp, xp_val, money, armor, strength):
+    def __init__(self, name, max_hp, xp_val, money, armor, strength, level):
         super.__init__(name)
         self.max_hp = max_hp
         self.name = name
@@ -9,3 +10,4 @@ class Monster(Character):
         self.strength = strength
         self.xp_val = xp_val
         self.money = money
+        self.level = level
