79959b94a11badba8d4b45c648947310d0212d29
robski577 <robski577@gmail.com>
2016-09-30 16:27:41 -0500

    so with the game frame i went with something simple, its not my final version but it'll do for now. This is just the frame I still have to add the save button and connect everything, which will happen tomorrow.

--- character/Game Frame.py
+++ character/Game Frame.py
@@ -1,7 +1,7 @@
-from battle_result import NumGen
+from character.Hero import Hero
+from character.Monster import *
+
 from tkinter import *
-import tkinter as tk
-import sqlite3
 import random
 import math
 
@@ -12,12 +12,12 @@ class GameDesign(Frame):
         self.master.title("GameDesign")
         self.grid()
 
-        self._heroLabel = Label(self, text="Hero")
-        self._heroLabel.grid(row=0, column=0)
-        self._heroStr = StringVar()
-        self._heroEntry = Entry(self,
-                                textvariable=self._heroStr)
-        self._heroEntry.grid(row=0, column=1)
+        self.HeroLabel = Label(self, text="Hero")
+        self.HeroLabel.grid(row=0, column=0)
+        self.HeroStr = StringVar()
+        self.HeroEntry = Entry(self,
+                                textvariable=self.HeroStr)
+        self.HeroEntry.grid(row=0, column=1)
 
         self._armorLabel = Label(self, text="armor")
         self._armorLabel.grid(row=2, column=0)
@@ -33,12 +33,12 @@ class GameDesign(Frame):
                                textvariable=self._strStr)
         self._strEntry.grid(row=1, column=1)
 
-        self._monsterLabel = Label(self, text="Monster")
-        self._monsterLabel.grid(row=0, column=2)
-        self._monsterStr = StringVar()
-        self._monsterEntry = Entry(self,
-                                   textvariable=self._monsterStr)
-        self._monsterEntry.grid(row=0, column=3)
+        self.MonsterLabel = Label(self, text="Monster")
+        self.MonsterLabel.grid(row=0, column=2)
+        self.MonsterStr = StringVar()
+        self.MonsterEntry = Entry(self,
+                                   textvariable=self.MonsterStr)
+        self.MonsterEntry.grid(row=0, column=3)
 
         self._monsterHpLabel = Label(self, text="Monster Hp")
         self._monsterHpLabel.grid(row=1, column=2)
@@ -71,39 +71,16 @@ class GameDesign(Frame):
         self._button = Button(self, text="Battle button", command=self._bpResults)
         self._button.grid(row=6, column=0, columnspan=1)
 
-    def _battle(self):
-        hero = int(input("Please enter a number up to 10. "))
-        monster = random.randint(1, 10)
-        """ x and z can both be used below without errors -- testing/learning"""
 
-        randomNumber = random.randint(1, 50)
-        print("hero attack does", randomNumber, "damg")
-        if hero < monster:
-            hero == ("winner")
-            print("winner")
-        elif hero > monster:
-            hero == ("lose")
-            print("lose")
-        else:
-            print("you lose")
+    #def c
 
-        randomNumber = random.randint(1, 50)
-        print("monster attack does", randomNumber, "damg")
-        if monster < hero:
-            monster == ("Winner")
-            print("winner")
-        elif monster > hero:
-            monster == ("lose")
-            print("lose")
-        else:
-            monster == ("monster defeated")
-            print("monster defeated")
+
 
     def _bpResults(self):
-        hero = self._heroStr.get()
+        hero = self.HeroStr.get()
         armor = self._armorStr.get()
         strenght = self._strStr.get()
-        monster = self._monsterStr.get()
+        monster = self.MonsterStr.get()
         monsterHp = self._monsterHpStr.get()
         location = self._locationStr.get()
         money = self._moneyStr.get()
