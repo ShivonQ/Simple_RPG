d5b4c70b143215540ca524fd1bdf08f19ede67a6
Federico <fedefdez1982@gmail.com>
2016-09-30 15:35:11 -0500

    The Merchant class, basically another character but with an inventory (to store the items that will be sold)
    Don't see the need of it having strength as he doesn't attack, just tries to flee if attacked

---
+++ character/Merchant.py
@@ -0,0 +1,21 @@
+from Character.Character import *
+
+
+class Merchant(Character):
+    def __init__(self, name, money, inventory):
+        super().__init__(name)
+        # self.name = name
+        # self.armor = armor
+        # self.strength = strength
+        self.money = money
+        self.inventory = inventory
+
+
+    def set_armor_max_hp(self, arm, max_hp):
+
+        self.armor = arm
+        self.max_hp = max_hp
+
+    def set_inventory (self, inventory):
+
+        self.inventory = inventory
